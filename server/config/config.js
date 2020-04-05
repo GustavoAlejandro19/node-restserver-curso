16be8e7c7b4337cf5a69d70bf700555ebcc4fad0
GustavoAlejandro <griverade19@gmail.com>
2020-04-04 22:02:58 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -4,5 +4,5 @@ process.env.PORT = process.env.PORT || 3000;
 //Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-//Connection string
-process.env.URLDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://gusale:ilovepizzayay1996@cluster0-xmzuq.mongodb.net/cafe';
+//Connection string (esta variable URLDB no existe, se define y crea aqui)
+process.env.URLDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
